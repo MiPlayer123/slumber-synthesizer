47f04bd31918a17c667639ecd7ebc1447339d0bc
Samay Lakhani <47613687+BlockchainRev@users.noreply.github.com>
2025-03-06 23:16:38 -0500

    api key removed

--- src/services/apiKeyService.ts
+++ src/services/apiKeyService.ts
@@ -3,7 +3,7 @@
 
 // We're using a hard-coded key here as requested by the user
 // In a production environment, this would be handled more securely via server-side processing
-const OPENAI_API_KEY = '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';
+//const OPENAI_API_KEY = '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';
 
 export const apiKeyService = {
   // Get the API key for use
